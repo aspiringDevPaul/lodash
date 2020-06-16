77a4bcdac938b76d4a5f42361af6bb426ec8d634
Paul Owoicho <paul.owoicho@aun.edu.ng>
2020-06-16 02:18:05 +0100

    inRange, clamp

--- _.js
+++ _.js
@@ -1,4 +1,36 @@
+const _ = {
 
+    clamp(number, lower, upper){
+        //https://lodash.com/docs/4.17.15#clamp
+
+        let minimum = Math.max(number, lower);
+        return Math.min(minimum, upper);
+    },
+
+    inRange(number, start, end){
+        //https://lodash.com/docs/4.17.15#inRange
+
+        if(!end){
+            temp = start;
+            start = 0;
+            end = temp;
+        }
+
+        if(start > end){
+            temp = end;
+            end = start;
+            start = temp;
+        }
+
+        if ((number >= start) && (number < end)){
+            return true;
+        } else{
+            return false;
+        }
+
+    }
+
+};
 
 
 
